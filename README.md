c10207c19e8e54a039a3fbb43a2975dd67aa4fc5
nurdankaya <nnurdankayaq@gmail.com>
2022-05-08 02:21:55 +0300

    get access token and make ocr request

---
+++ README.md
@@ -0,0 +1,8 @@
+
+pip install pillow
+python manage.py makemigrations 
+python manage.py migrate
+pip install --upgrade imutils
+
+
+pip install tensorflow
--- webui/views.py
+++ webui/views.py
@@ -1,23 +1,13 @@
-from sys import version
-from traceback import print_tb
 from django.http import JsonResponse
 from django.shortcuts import render
 
 from webui.models import Image
-
-import pytesseract as tess
-tess.pytesseract.tesseract_cmd = r'C:\Users\coban\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
-
-from pytesseract import image_to_string
-
 import cv2
-import imutils
-from imutils.perspective import four_point_transform
-
-from requests import options
 import tensorflow as tf
 import numpy as np
 
+import json
+import requests
 # Create your views here.
 
 def home(request):
@@ -40,85 +30,22 @@ def upload_file(request):
     return render(request, template_name, context=context)
 
 
-def improve(imageurl, lang):
-    # Source CODE
-    "https://www.pyimagesearch.com/2021/10/27/automatically-ocring-receipts-and-scans/"
-    orig = cv2.imread(imageurl)
-    image = orig.copy()
-    image = imutils.resize(image, width=500)
-    ratio = orig.shape[1] / float(image.shape[1])
-
-    # convert the image to grayscale, blur it slightly, and then apply
-    # edge detection
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    blurred = cv2.GaussianBlur(gray, (5, 5,), 0)
-    edged = cv2.Canny(blurred, 75, 200)
-    # check to see if we should show the output of our edge detection
-    # procedure
-
-    # find contours in the edge map and sort them by size in descending
-    # order
-    cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL,
-                            cv2.CHAIN_APPROX_SIMPLE)
-    cnts = imutils.grab_contours(cnts)
-    cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
-
-    # initialize a contour that corresponds to the receipt outline
-    receiptCnt = None
-    # loop over the contours
-    for c in cnts:
-        # approximate the contour
-        peri = cv2.arcLength(c, True)
-        approx = cv2.approxPolyDP(c, 0.02 * peri, True)
-        # if our approximated contour has four points, then we can
-        # assume we have found the outline of the receipt
-        if len(approx) == 4:
-            receiptCnt = approx
-            break
-    # if the receipt contour is empty then our script could not find the
-    # outline and we should be notified
-    if receiptCnt is None:
-        raise Exception(("Could not find receipt outline. "
-                         "Try debugging your edge detection and contour steps."))
-
-    output = image.copy()
-
-    cv2.drawContours(output, [receiptCnt], -1, (0, 255, 0), 2)
-
-    # apply a four-point perspective transform to the *original* image to
-    # obtain a top-down bird's-eye view of the receipt
-    receipt = four_point_transform(orig, receiptCnt.reshape(4, 2) * ratio)
-
-    # apply OCR to the receipt image by assuming column data, ensuring
-    # the text is *concatenated across the row* (additionally, for your
-    # own images you may need to apply additional processing to cleanup
-    # the image, including resizing, thresholding, etc.)
-    options = "--psm 4"
-    text = image_to_string(cv2.cvtColor(
-        receipt, cv2.COLOR_BGR2RGB), lang=lang,  config=options)
-
-    return text
-
-
 def ocr(request):
     print("ocr metoduna geldik")
     context = {"success": False}
     try:
         imageid = request.POST["imageid"]
         version = request.POST["version"]
-      
+    
         coords_font = request.POST["coords_font"]
         coords_x = request.POST["coords_x"]
         coords_y = request.POST["coords_y"]
         coords_w = request.POST["coords_w"]
         coords_h = request.POST["coords_h"]
 
+
         image = Image.objects.filter(imageid=imageid).get()
         path = image.Image.url[1:]
-
-        #token alınacak
-
-
 
         #resmi gelen koordinatlara göre kırpalım
         img = cv2.imread(path)
@@ -140,17 +67,54 @@ def ocr(request):
 
 
 
-        #result = improve(path, version)
 
-        gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-        ret2, th2 = cv2.threshold(
-            gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-        img = cv2.fastNlMeansDenoising(th2, 10, 10, 7)
-        result = image_to_string(img, lang=version)
-        context["text"] = result
+
+
+        ################take token
+
+
+        user_data = {
+            'username': 'test1234',
+            'password': 'test1234',
+        }
+
+        token_url = "https://www.osmanlica.com/api/token/"
+
+        r = requests.post(token_url, user_data)
+       
+        resp_dict = json.loads(r.text)
+        access_token = resp_dict['access']
+
+        print("TOKEN ", access_token)
+
+
+         ################make ocr with access token
+
+        image_url = "http://www.mervecoban.me/images/cropped/image.jpg"  # give exact path of image when hosted
+
+        ocr_data = {
+            'username': 'test1234',
+        }
+        ocr_data['image_url'] = image_url
+
+        ocr_headers = {
+            'Content-Type': 'application/json',
+        }
+        ocr_headers['Authorization'] = "Bearer " + access_token
+
+        ocr_url = "https://www.osmanlica.com/api/image_line"
+
+        r = requests.post(ocr_url, json = ocr_data, headers = ocr_headers)
+
+        resp_dict = json.loads(r.text)
+        image_detail = resp_dict['image_detail']
+        print("image_detail OCR RESULT ", image_detail)
+
+        context["text"] =  str(image_detail)
         context["success"] = True
-    except Exception as identifier:
-        context["message"] = "ERROR"
+
+    except requests.exceptions.HTTPError as e:
+        context["message"] = "What went wrong is ", str(e)
 
     return JsonResponse(context, safe=False)
 
