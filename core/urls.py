fa1a4b3a0e33f98203a888745d22993f4e429a92
nurdankaya <nnurdankayaq@gmail.com>
2022-05-24 00:29:00 +0300

    update media urls.py and senttings.py

--- core/urls.py
+++ core/urls.py
@@ -15,13 +15,15 @@ Including another URLconf
 """
 from django.conf.urls import include
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from django.conf.urls.static import static
 from . import settings
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("webui.urls")),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
 
